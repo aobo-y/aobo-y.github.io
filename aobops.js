23bc9c4a1a968374b2a38c0074ec38a25d2a1d51
Yang Aobo <yangaobo@gmail.com>
2014-05-07 20:55:02 +0800

    second commit

--- aobops.js
+++ aobops.js
@@ -1,19 +1,54 @@
 $(document).ready(function(){
-	initializeScrollAnimation();
+	setHeaderHeight();
+	initializeNavbarButton();
+	initializeResumeScroll();
 	initializeGoogleMap();
 });
 
+function setHeaderHeight(){
+	var h=$(window).height()-$('#me').outerHeight();
+	$('#header').height(h+'px');
+}
 
-function initializeScrollAnimation(){
+function initializeNavbarButton(){
 	$("#nav-btn-list a, .navbar-brand").click(function(){
 		
 		$("html, body").animate({
-			scrollTop: $(this.getAttribute("href")).offset().top-20
+			scrollTop: $(this.getAttribute("href")).offset().top-50
 		},300)
 		return false;
 	})	
 }
 
+function initializeResumeScroll(){
+	$('#resume-catalog').height($('#resume-content').height());
+	var resumeBegin=$('#resume').offset().top-50;
+	var resumeEnd=$('#work').offset().top-375;
+	$(window).scroll(function(e){
+		var p=$(window).scrollTop();
+		if(p>=resumeBegin&&p<=resumeEnd){
+			var ul=$('#resume-catalog ul');
+			if(!ul.hasClass('fixed')){
+				ul.removeClass();
+				ul.addClass('fixed');
+			}
+		}
+		else if(p<resumeBegin){
+			var ul=$('#resume-catalog ul');
+			if(!ul.hasClass('absolute-top')){
+				ul.removeClass();
+				ul.addClass('absolute-top');
+			}			
+		}
+		else{
+			var ul=$('#resume-catalog ul');
+			if(!ul.hasClass('absolute-bottom')){
+				ul.removeClass();
+				ul.addClass('absolute-bottom');
+			}			
+		}
+	});
+}
 
 function initializeGoogleMap() {
   var mapOptions = {
