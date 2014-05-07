3bde7a684b063c5da6a41a9a34d6f1d98765e08e
Yang Aobo <yangaobo@gmail.com>
2014-05-07 23:38:02 +0800

    gradualanimation commit

--- aobops.js
+++ aobops.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	setHeaderHeight();
+	initializeSocialIconHover();
 	initializeNavbarButton();
 	initializeResumeScroll();
 	initializeGoogleMap();
@@ -11,13 +12,53 @@ function setHeaderHeight(){
 }
 
 function initializeNavbarButton(){
-	$("#nav-btn-list a, .navbar-brand").click(function(){
+	$('#nav-btn-list a, .navbar-brand').click(function(){
 		
-		$("html, body").animate({
-			scrollTop: $(this.getAttribute("href")).offset().top-50
+		$('html, body').animate({
+			scrollTop: $(this.getAttribute('href')).offset().top-50
 		},300)
 		return false;
 	})	
+}
+
+function circleEleResize(ele,size){
+	ele.stop().animate({
+		overflow: 'none',
+		width: size,
+		height: size
+	},'fast');	
+}
+
+function gradualIncreaSize(ele,size,direction,offset){
+	offset=offset||0;
+	var step=5-offset;
+	circleEleResize(ele,size);
+	if(size-step>50){
+		if(direction==0||direction==2){
+			var $prev=ele.prev();
+			if($prev){
+				gradualIncreaSize($prev,size-step,0,offset+1);
+			}
+		}
+		if(direction==1||direction==2){
+			var $next=ele.next();
+			if($next){
+				gradualIncreaSize($next,size-step,1,offset+1);
+			}	
+		}		
+	}
+
+}
+
+function initializeSocialIconHover(){
+	$('.circle-social-icon').hover(function(){
+		$this=$(this);
+		gradualIncreaSize($this,62,2);
+
+	},function(){
+		$this=$(this);
+		circleEleResize($('.circle-social-icon'),50);		
+	});
 }
 
 function initializeResumeScroll(){
@@ -58,6 +99,6 @@ function initializeGoogleMap() {
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  $("#contact-right").height($("#contact").height());
+  $('#contact-right').height($('#contact').height());
 }
 
