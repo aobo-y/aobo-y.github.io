5828c794762129088e380335cc0ef9cb2a09130b
Yang Aobo <yangaobo@gmail.com>
2014-05-08 21:01:30 +0800

    skill_part commit

--- aobops.js
+++ aobops.js
@@ -13,7 +13,6 @@ function setHeaderHeight(){
 
 function initializeNavbarButton(){
 	$('#nav-btn-list a, .navbar-brand').click(function(){
-		
 		$('html, body').animate({
 			scrollTop: $(this.getAttribute('href')).offset().top-50
 		},300)
@@ -31,19 +30,19 @@ function circleEleResize(ele,size){
 
 function gradualIncreaSize(ele,size,direction,offset){
 	offset=offset||0;
-	var step=5-offset;
+	var step=4-offset;
 	circleEleResize(ele,size);
 	if(size-step>50){
 		if(direction==0||direction==2){
 			var $prev=ele.prev();
 			if($prev){
-				gradualIncreaSize($prev,size-step,0,offset+1);
+				gradualIncreaSize($prev,size-step,0);
 			}
 		}
 		if(direction==1||direction==2){
 			var $next=ele.next();
 			if($next){
-				gradualIncreaSize($next,size-step,1,offset+1);
+				gradualIncreaSize($next,size-step,1);
 			}	
 		}		
 	}
@@ -63,31 +62,78 @@ function initializeSocialIconHover(){
 
 function initializeResumeScroll(){
 	$('#resume-catalog').height($('#resume-content').height());
-	var resumeBegin=$('#resume').offset().top-50;
-	var resumeEnd=$('#work').offset().top-375;
+	var resumeBegin=$('#resume').offset().top;
+	var resumeEnd=$('#skill-section').offset().top+40-10;
+
+	var eduBegin=$('#education-section').offset().top-80;
+	var jobBegin=$('#job-section').offset().top-30;
+	var awardBegin=$('#award-section').offset().top-20;
+	var skillBegin=$('#skill-section').offset().top-20;
+	
+	var ul=$('#resume-catalog ul');
+	var lis=$('#resume-catalog li');
+
+	$('.cl-item a').click(function(){
+		$('html, body').animate({
+			scrollTop: $(this.getAttribute('href')).offset().top-40
+		},300)
+		return false;
+	})	
+
 	$(window).scroll(function(e){
-		var p=$(window).scrollTop();
+		var p=$(window).scrollTop()+50;
 		if(p>=resumeBegin&&p<=resumeEnd){
-			var ul=$('#resume-catalog ul');
+			
 			if(!ul.hasClass('fixed')){
 				ul.removeClass();
 				ul.addClass('fixed');
 			}
 		}
 		else if(p<resumeBegin){
-			var ul=$('#resume-catalog ul');
 			if(!ul.hasClass('absolute-top')){
 				ul.removeClass();
 				ul.addClass('absolute-top');
 			}			
 		}
 		else{
-			var ul=$('#resume-catalog ul');
 			if(!ul.hasClass('absolute-bottom')){
 				ul.removeClass();
 				ul.addClass('absolute-bottom');
 			}			
 		}
+
+		if(p>=eduBegin&&p<=resumeEnd+250){
+			if(p<jobBegin){
+				var cli=$('#cl-item-edu');
+				if(!cli.hasClass('highlighted')){
+					lis.removeClass('highlighted');
+					cli.addClass('highlighted');
+				}
+			}
+			else if(p<awardBegin){
+				var cli=$('#cl-item-job');
+				if(!cli.hasClass('highlighted')){
+					lis.removeClass('highlighted');
+					cli.addClass('highlighted');
+				}
+			}
+			else if(p<skillBegin){
+				var cli=$('#cl-item-award');
+				if(!cli.hasClass('highlighted')){
+					lis.removeClass('highlighted');
+					cli.addClass('highlighted');
+				}
+			}
+			else{
+				var cli=$('#cl-item-skill');
+				if(!cli.hasClass('highlighted')){
+					lis.removeClass('highlighted');
+					cli.addClass('highlighted');
+				}
+			}			
+		}
+
+
 	});
 }
 
