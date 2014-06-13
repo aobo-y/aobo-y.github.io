8956d7b7616668133e08d2698ca7aa9736201ad2
Yang Aobo <yangaobo@gmail.com>
2014-06-13 20:53:35 +0800

    overall improve

--- aobops.js
+++ aobops.js
@@ -1,5 +1,5 @@
 var navbarHeight;
-
+var WEB_SERVICE='http://yangaobo.appspot.com';
 var $resumeClUl;
 var $resumeClLi;	
 
@@ -11,19 +11,71 @@ var awardBegin;
 var skillBegin;
 
 $(document).ready(function(){
+
 	initializeVariables();
 
 	setHeaderHeight();
+	setResumeCatalogHeight();
+
 	addWindowSizeChangeHandler();
 	addSocialIconHoverHandler();
 	addNavbarBtnHandler();
 	addFindMoreBtnHandler();
 	addResumeCatalogClickHandler();
+	addContactFormSubmitBtnClickHandler();
 
 	initializeResumePartBorderVar();
 	addScrollHandler();
 	initializeGoogleMap();
 });
+
+
+function addContactFormSubmitBtnClickHandler(){
+	var contactForm=$('#contact form');
+	contactForm.find('button').click(function(evt){
+		evt.preventDefault();
+		var hasError=false;
+		var inputBox=contactForm.find('input, textarea').each(function(index){
+			var $ele=$(this);
+
+			if($ele.val().trim()===''){
+				$ele.parent().removeClass('has-success').addClass('has-error');
+				hasError=true;
+				$ele.next('.form-control-feedback').removeClass('glyphicon-ok').addClass('glyphicon-remove').show();
+			}
+			else{
+				$ele.parent().removeClass('has-error has-success');
+				$ele.next('.form-control-feedback').hide();
+			}
+		});
+
+		if(hasError){
+			return;
+		}
+
+		$.ajax({
+			url: WEB_SERVICE+'/send_message',
+			type: 'post',
+			data: contactForm.serialize(),
+			success: function(data){
+				if(data){
+					inputBox.each(function(index){
+						var $ele=$(this);
+						$ele.parent().removeClass('has-error').addClass('has-success');
+						$ele.next('.form-control-feedback').removeClass('glyphicon-remove').addClass('glyphicon-ok').show();
+					});
+					alert('Thank you for your message');
+				}
+				else{
+					alert('Sever error: Send failed, please try it later');
+				}
+			},
+			error: function(jqXHR, textStatus){
+				alert(textStatus+': Send failed, please try it later');			
+			}
+		});
+	});
+}
 
 function initializeVariables(){
 	navbarHeight=$('#navbar').height();
@@ -38,11 +90,10 @@ function setHeaderHeight(){
 		h-=$('#me').outerHeight()
 	}
 	$('#header').outerHeight(h);
+}
 
-	var hc=$('#header-content');
-	hc.css({
-		'margin-top':'-'+ hc.height()/2 + 'px'
-	})
+function setResumeCatalogHeight(){
+	$('#resume-catalog').height($('#resume-content').height());
 }
 
 function animateScrollTo(destination){
@@ -123,87 +174,107 @@ function addSocialIconHoverHandler(){
 
 function initializeResumePartBorderVar(){	
 	resumeBegin=$('#resume').offset().top;
-	resumeEnd=$('#skill-section').offset().top+40-10;
+	//200 is the height to bottom; 80 is the fixed height to top; last is the height of catalog including margin
+	resumeEnd=$('#work').offset().top-200-80-$('#resume-catalog > ul').outerHeight(true);
 
-	eduBegin=$('#education-section').offset().top-80;
+	eduBegin=$('#education-section').offset().top-150;
 	jobBegin=$('#job-section').offset().top-30;
 	awardBegin=$('#award-section').offset().top-20;
 	skillBegin=$('#skill-section').offset().top-20;
 }
 
-function updateResumeCatalog(){
+function updateResumeCatalog(p){
 	var ul=$resumeClUl;
 	var lis=$resumeClLi;
+	var statusClass;
 
-	var p=$(window).scrollTop()+navbarHeight;
-	if(p>=resumeBegin&&p<=resumeEnd){
-		
-		if(!ul.hasClass('fixed')){
-			ul.removeClass();
-			ul.addClass('fixed');
-		}
-	}
-	else if(p<resumeBegin){
-		if(!ul.hasClass('absolute-top')){
-			ul.removeClass();
-			ul.addClass('absolute-top');
-		}			
-	}
-	else{
-		if(!ul.hasClass('absolute-bottom')){
-			ul.removeClass();
-			ul.addClass('absolute-bottom');
-		}			
+	switch (true){
+		case (p<resumeBegin):
+			statusClass='absolute-top'
+			break;
+		case (p<=resumeEnd):
+			statusClass='fixed';
+			break;
+		default:
+			statusClass='absolute-bottom';
+			break;
 	}
 
-	if(p>=eduBegin&&p<=resumeEnd+250){
-		if(p<jobBegin){
-			var cli=$('#cl-item-edu');
-			if(!cli.hasClass('highlighted')){
-				lis.removeClass('highlighted');
-				cli.addClass('highlighted');
-			}
-		}
-		else if(p<awardBegin){
-			var cli=$('#cl-item-job');
-			if(!cli.hasClass('highlighted')){
-				lis.removeClass('highlighted');
-				cli.addClass('highlighted');
-			}
-		}
-		else if(p<skillBegin){
-			var cli=$('#cl-item-award');
-			if(!cli.hasClass('highlighted')){
-				lis.removeClass('highlighted');
-				cli.addClass('highlighted');
-			}
-		}
-		else{
-			var cli=$('#cl-item-skill');
-			if(!cli.hasClass('highlighted')){
-				lis.removeClass('highlighted');
-				cli.addClass('highlighted');
-			}
-		}			
+	if(!ul.hasClass(statusClass)){
+		ul.removeClass();
+		ul.addClass(statusClass);
 	}
+
+	if(p>=eduBegin&&p<=resumeEnd){
+		var cli;
+		var index;
+		switch (true){
+			case (p<jobBegin):
+				index=0;
+				break;
+			case (p<awardBegin):
+				index=1;
+				break;
+			case (p<skillBegin):
+				index=2;
+				break;
+			default:
+				index=3;
+				break;
+		}
+
+		cli=lis.eq(index);
+
+		if(!cli.hasClass('highlighted')){
+			lis.removeClass('highlighted');
+			cli.addClass('highlighted');
+		}
+	}
+
 }
 
 function addScrollHandler(){
-	$('#resume-catalog').height($('#resume-content').height());
 
 	$(window).scroll(function(e){
-		updateResumeCatalog();
+		var topLine=$(window).scrollTop()+navbarHeight;
+
+		updateResumeCatalog(topLine);
 	});
 }
 
 function initializeGoogleMap() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(22.303325,114.186659)
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+	var markerAdded=false;
+	var myLatlng = new google.maps.LatLng(22.303325,114.186659);
+	var mapOptions = {
+		zoom: 8,
+		center: myLatlng
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  $('#contact-right').height($('#contact').height());
+
+	$(window).scroll(function(e){
+
+		var bottomLine=$(window).scrollTop()+$(window).height();
+		var $mapCanvas=$('#map-canvas');
+		if(bottomLine>=$mapCanvas.offset().top+400&&(!markerAdded)){
+			var marker = new google.maps.Marker({
+				position: myLatlng,
+    			animation: google.maps.Animation.DROP,
+				map: map,
+				title: 'Spend most of time here'
+			});
+			google.maps.event.addListener(marker, 'mouseover', function(){
+				if (marker.getAnimation() == null) {
+					marker.setAnimation(google.maps.Animation.BOUNCE);
+					setTimeout(function(){marker.setAnimation(null);}, 5500);
+				}
+			});
+
+			markerAdded=true;
+		}
+		
+	});
+
+  	$('#contact-right').height($('#contact').height());
 }
 
