d0506a23bdca8d91a6119d9c2848c4d18ce3b458
Aobo Yang <yangaobo@gmail.com>
2017-12-24 17:41:02 +0800

    rewrite Resume section

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,33 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 const EXPERIENCES = [
   {
     position: 'Software Engineer',
+    organization: 'Aftership Ltd',
+    location: 'Hong Kong',
+    duration: 'Jul 2015 - Now',
+    desc: 'Lead the web development'
+  },
+  {
+    position: 'Software Engineer',
     organization: 'Harmonic Inc',
     location: 'Hong Kong',
-    duration: 'Aug 2014 - Now',
-    desc: 'Worked in the cloud team to develop a software-based media processing platform as the next generation proudct for the company.'
+    duration: 'Aug 2014 - Jul 2015',
+    desc: 'Built the RESTful API and web portal of the customer cloud system'
   },
   {
     position: 'Graduate Trainee',
     organization: 'PCCW Solutions',
     location: 'Hong Kong',
     duration: 'Aug 2012 - Jan 2014',
-    desc: 'Employed as Graduate Trainee by PCCW Solutions. I worked as a programmer in the development of a Java System for Social Security Department of HKSAR Government. Working in data conversion team, I help to collect, migrate and convert the raw data into the system.'
+    desc: 'Handled data conversion for the system of Social Security Department, HKSAR Government'
   },
   {
     position: 'Developer Intern',
     organization: 'Perugia University',
     location: 'Italy',
     duration: 'Jun 2012 - Aug 2012',
-    desc: 'Joined IT department of the university as an intern and responsible to research on building mobile applications with web technologies to cross platforms and develop such mobile web applications for campus.'
+    desc: 'Built cross-platform mobile applications in web technologies, with Cordova and jQuery'
   },
   {
     position: 'Placement',
     organization: 'HSBC',
     location: 'Hong Kong',
     duration: 'May 2010 - May 2011',
-    desc: 'Worked as a placement in the Enterprise Architecture team.  My main duties were to develop processes to automatically analyze data and generate reports to support the management.'
+    desc: 'Delivered programs for the visualization of enterprise architecture data'
   }
 ]
 
@@ -52,120 +61,100 @@ const SKILLS = [
   'MongoDB'
 ]
 
+const ResumeRow = ({ label, title, org, date, desc }) => {
+  return (
+    <Row className='resume__row'>
+      <Col md={3} className='text-center'>
+        {label}
+      </Col>
+      <Col md={6}>
+        {title}
+        <br />
+        <i>{org}</i>
+      </Col>
+      <Col md={3} className='text-center'>
+        {date}
+      </Col>
+      {
+        desc &&
+          <Col md={6} mdOffset={3} className='resume__row__desc'>
+            <p className='text-muted'>
+              {desc}
+            </p>
+          </Col>
+      }
+    </Row>
+  )
+}
+
+ResumeRow.propTypes = {
+  label: PropTypes.string,
+  title: PropTypes.string,
+  org: PropTypes.string,
+  date: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  desc: PropTypes.string
+}
+
 const Resume = () => {
   return (
-    <div id='resume'>
-      <div className='container'>
-        <div className='row'>
-          <div id='resume-content' className='col-lg-10 col-sm-12'>
-            <div className='container-fluid'>
-              <section id='education-section' name='education' />
-              <div className='row'>
-                <div className='col-md-2'>
-                  <p className='resume-item-name'>EDUCATION</p>
-                </div>
-
-                <div className='col-md-7'>
-                  <p><t>Bachelor of Science in Computing Studies</t><br />
-                    Hong Kong Baptist University<br />
-                  </p>
-                </div>
-                <div className='col-md-3'>
-                  <p>Sep 2007 - May 2012 <br />
-                    Suspended in 2010 for placement</p>
-                </div>
-              </div>
-
-              <section id='job-section' name='job' />
-              <hr />
-              {
-                EXPERIENCES.map((exp, index) => (
-                  <div key={index} className='row'>
-                    <div className='col-md-2'>
-                      <p className='resume-item-name'>JOB</p>
-                    </div>
-                    <div className='col-md-7'>
-                      <t>{exp.position}</t>
-                      <br />
-                      {exp.organization}
-                    </div>
-                    <div className='col-md-3'>
-                      {exp.duration}
-                    </div>
-                    <div className='col-md-7 col-md-offset-2'>
-                      <p className='resume-description'>
-                        {exp.desc}
-                      </p>
-                    </div>
+    <section className='section--white'>
+      <Grid>
+        <ResumeRow
+          label='EDUCATION'
+          title='Bachelor of Science in Computing Studies'
+          org='Hong Kong Baptist University'
+          date={
+            <span>
+              Sep 2007 - May 2012
+              <br />
+              <span className='text-muted'>Job placement in 2010</span>
+            </span>
+          }
+        />
+        <hr />
+        {
+          EXPERIENCES.map((exp, index) => (
+            <ResumeRow
+              key={index}
+              label={index === 0 ? 'JOB' : null}
+              title={exp.position}
+              org={`${exp.organization}, ${exp.location}`}
+              date={exp.duration}
+              desc={exp.desc}
+            />
+          ))
+        }
+        <hr />
+        <ResumeRow
+          label='AWARD'
+          title='Silver Award'
+          org='HKEIA Innovation & Technology Project Competition'
+          date='Nov 2012'
+          desc='Held by Hong Kong Electronic Industries Association and aims to recognize and reward bachelor or higher diploma graduates of electronic engineering or related engineering fields with outstanding projects demonstrating excellence in technology and innovation'
+        />
+        <hr />
+        <Row>
+          <Col md={3} className='text-center'>
+            SKILL
+          </Col>
+          <Col md={7}>
+            {MAIN_SKILLS.map((skill, index) => (
+              <div key={index} className='main-skill-bar'>
+                <p>{skill.name}</p>
+                <div className='progress progress-striped'>
+                  <div className='progress-bar progress-bar-info' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style={{width: `${skill.progress}%`}}>
+                    <span className='sr-only'>{skill.progress}% Complete</span>
                   </div>
-                ))
-              }
-
-              <section id='award-section' name='award' />
-              <hr />
-
-              <div className='row'>
-                <div className='col-md-2'>
-                  <p className='resume-item-name'>AWARD</p>
-                </div>
-                <div className='col-md-7'>
-                  <p><t>Silver Award</t><br />
-                    HKEIA Innovation & Technology Project Competition<br />
-                  </p>
-                </div>
-                <div className='col-md-3'>
-                  <p>Nov 2012</p>
                 </div>
               </div>
-
-              <div className='row'>
-                <div className='col-md-7 col-md-offset-2'>
-                  <p className='resume-description'>Held by Hong Kong Electronic Industries Association and aims to recognize and reward bachelor or higher diploma graduates of electronic engineering or related engineering fields with outstanding projects demonstrating excellence in technology and innovation</p>
-                </div>
-              </div>
-
-              <section id='skill-section' name='skill' />
-              <hr />
-
-              <div className='row'>
-                <div className='col-md-2'>
-                  <p className='resume-item-name'>SKILL</p>
-                </div>
-                <div className='col-md-9'>
-                  {MAIN_SKILLS.map((skill, index) => (
-                    <div key={index} className='main-skill-bar'>
-                      <p>{skill.name}</p>
-                      <div className='progress progress-striped'>
-                        <div className='progress-bar progress-bar-info' role='progressbar' aria-valuenow='70' aria-valuemin='0' aria-valuemax='100' style={{width: `${skill.progress}%`}}>
-                          <span className='sr-only'>{skill.progress}% Complete</span>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <hr className='invisible-hr' />
-              <div className='row'>
-                <div className='col-md-9 col-md-offset-2'>
-                  {SKILLS.map((skill, index) => <p key={index} className='skill-tag'>{skill}</p>)}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div id='resume-catalog' className='col-lg-2 visible-lg'>
-            <ul>
-              <li id='cl-item-edu' className='cl-item'><a href='#resume'>Education</a></li>
-              <li id='cl-item-job' className='cl-item'><a href='#job-section'>Jobs</a></li>
-              <li id='cl-item-award' className='cl-item'><a href='#award-section'>Awards</a></li>
-              <li id='cl-item-skill' className='cl-item'><a href='#skill-section'>Skills</a></li>
-            </ul>
-          </div>
-
-        </div>
-      </div>
-
-    </div>
+            ))}
+          </Col>
+          <Col md={7} mdOffset={3}>
+            {SKILLS.map((skill, index) => <p key={index} className='skill-tag'>{skill}</p>)}
+          </Col>
+        </Row>
+      </Grid>
+    </section>
   )
 }
 
