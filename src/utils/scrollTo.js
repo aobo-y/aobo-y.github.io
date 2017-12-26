09be48a869ccdec621b3ae00d450944f2f639e24
Aobo Yang <yangaobo@gmail.com>
2017-12-26 17:29:58 +0800

    implement smooth scroll

--- src/utils/scrollTo.js
+++ src/utils/scrollTo.js
@@ -1,11 +1,41 @@
 function scrollTo (name) {
-  if (!name) return window.scrollTo(0, 0)
+  if (!name) return smoothScroll(0, 0)
 
   const element = document.getElementById('section-' + name)
   if (!element) return
 
   const offset = element.offsetTop
-  window.scrollTo(0, offset - 50)
+  smoothScroll(offset - 50)
+}
+
+let appScrollInterval
+
+function smoothScroll (offset) {
+  if (appScrollInterval) {
+    clearInterval(appScrollInterval)
+    appScrollInterval = null
+  }
+
+  const currentOffset = window.pageYOffset
+  const duration = 350
+  const frame = 35
+  const step = Math.round((offset - currentOffset) / frame)
+
+  appScrollInterval = setInterval(() => {
+    const target = window.pageYOffset + step
+
+    if (
+      (target === offset) ||
+      (step > 0 && target > offset) ||
+      (step < 0 && target < offset)
+    ) {
+      window.scrollTo(0, offset)
+      clearInterval(appScrollInterval)
+      appScrollInterval = null
+    } else {
+      window.scrollTo(0, target)
+    }
+  }, duration / frame)
 }
 
 export default scrollTo
